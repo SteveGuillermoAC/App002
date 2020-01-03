6ab168f3c67ff4075a89f6ffe5e6454396e37ddb
SteveGuillermoAC <45379600+SteveGuillermoAC@users.noreply.github.com>
2020-01-03 14:12:26 -0500

    Initial commit TAREA DOS, VENTANAS, SIN ACCION A LOS BOTONES

---
+++ app/src/main/java/ec/edu/tecnologico/app2/MainActivity.java
@@ -0,0 +1,20 @@
+package ec.edu.tecnologico.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void cambiarVentana2(){
+
+
+
+    }
+}
