cf993f802ca30d7fff8d07a274646477e5016ee7
SteveGuillermoAC <45379600+SteveGuillermoAC@users.noreply.github.com>
2020-01-07 00:04:40 -0500

    agregado acciones

--- app/src/main/java/ec/edu/tecnologico/app2/MainActivity.java
+++ app/src/main/java/ec/edu/tecnologico/app2/MainActivity.java
@@ -2,19 +2,27 @@ package ec.edu.tecnologico.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void cambiarVentana2(){
 
+        botonCheck= (Button) findViewById(R.id.botonC);
 
-
+        botonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Ventana2.class));
+            }
+        });
     }
 }
--- app/src/main/java/ec/edu/tecnologico/app2/Ventana2.java
+++ app/src/main/java/ec/edu/tecnologico/app2/Ventana2.java
@@ -2,13 +2,26 @@ package ec.edu.tecnologico.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Ventana2 extends AppCompatActivity {
 
+    Button botonRegresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana2);
+
+        botonRegresar =(Button) findViewById(R.id.botonV);
+        botonRegresar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Ventana2.this,MainActivity.class));
+            }
+        });
     }
 }
